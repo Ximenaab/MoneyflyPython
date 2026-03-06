635318fb42265f8ebb75c91a5644a1130d2c0fd7
Ximena Beltrán <amelrosereds@gmail.com>
2026-03-05 20:23:28 -0500

    feat: Menu interactivo y logica basico del proyecto para registro y analisis de gastos

--- src/main.py
+++ src/main.py
@@ -1,33 +1,102 @@
+# Función que muestra el menú del programa
 def mostrar_menu():
-    print=("\n===MONEYFLY - CONTROL DE TUS GASTOS HORMIGA===")
-    print=("1.Registro gasto")
-    print=("2. Ver gastos")
-    print=("3. Ver total gastado")
-    print=("4. Ver promedio de gastos")
-    print=("5. Salir")
+    print("\n=== MONEYFLY - CONTROLA TUS GASTOS HORMIGA ===")
+    print("1. Registrar gasto")
+    print("2. Ver gastos")
+    print("3. Ver total gastado")
+    print("4. Ver promedio de gastos")
+    print("5. Salir")
+
+
+def registrar_gasto(gastos):
+
+    valor = float(input("Ingrese el valor del gasto: "))
+
+    categoria = input("Ingrese la categoría del gasto: ")
+
+    descripcion = input("Ingrese una breve descripción del gasto: ")
+
+    gasto = {
+        "valor": valor,
+        "categoria": categoria,
+        "descripcion": descripcion
+    }
+
+    gastos.append(gasto)
+
+    print("Gasto registrado correctamente")
+
+
+def ver_gastos(gastos):
+
+    if not gastos:
+          print("Aún no has registrado gastos, ¡empieza ahora!")
+          return
+    print("\nLista de gastos:")
+
+    for gasto in gastos:
+            print(f"Valor: {gasto['valor']} | Categoria: {gasto['categoria']} | Descripción: {gasto['descripcion']}")
+
+
+def calcular_total(gastos):
+
+    if not gastos:
+        print("No hay gastos registrados.")
+        return
+
+    total = sum(gasto["valor"] for gasto in gastos)
+    print("Total gastado:", total)
+
+
+
+def calcular_promedio(gastos):
+
+    if not gastos:
+        print("No hay gastos para calcular promedio")
+        return
+
+    total = sum(gasto["valor"] for gasto in gastos)
+
+    promedio = total / len(gastos)
+
+    print("Promedio de gastos:", promedio)
+
+
 
 def main():
+
+    # Lista gastos
     gastos = []
 
-while True:
-    mostrar_menu()
-    opcion = input("Hola! :) Seleccione una opción: ").strip()
-    if opcion == "1":
-        print("Registrar gasto(pdte)")
-    elif opcion == "2":
-        print ("Ver gastos(pdte)")
-    elif opcion == "3":
-        print ("Ver total(pdte)")
-    elif opcion == "4":
-        print ("Ver promedio")
-    elif opcion == "5":
-        print ("Salir...")
-        break
-    else: 
-        print("¡Ooops! Marca una opción válida")
+    
+    while True:
 
-    if __name__ == "__main__":
-         main()
+        mostrar_menu()
 
+        opcion = input("Seleccione una opción: ").strip()
+
+        if opcion == "1":
+            registrar_gasto(gastos)
+
+        elif opcion == "2":
+            ver_gastos(gastos)
+
+        elif opcion == "3":
+            calcular_total(gastos)
+
+        elif opcion == "4":
+            calcular_promedio(gastos)
+
+        elif opcion == "5":
+            print("Saliendo del programa...")
+            break
+
+        else:
+            print("Oopss! Marca una opción válida")
+
+
+# Punto de entrada del programa
+if __name__ == "__main__":
+    main()
 
 
